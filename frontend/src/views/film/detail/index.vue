7e0a71d70874d858037c1777ea7dfd54f1b484ff
lixiang <lixiang@qq.com>
2023-12-14 23:04:51 +0800

    加入評論區

--- frontend/src/views/film/detail/index.vue
+++ frontend/src/views/film/detail/index.vue
@@ -1,37 +1,72 @@
 <template>
-  <div class="subject clearfix subjectwrap">
-    <h1>
-      <span>{{ film.movieName }}</span>
-    </h1>
-    <div class="movie-container">
-      <img :src="film.picUrl" alt="瞒天过海" title="点击看更多海报" class="movie-poster" />
-    </div>
-    <div class="info">
-      <div class="flex-item">
-        <span class="pl">类型:</span> <span>{{ film.genre }}</span>
+ <div>
+      <div class="subject clearfix subjectwrap">
+        <h1>
+          <span>{{ film.movieName }}</span>
+        </h1>
+        <div class="movie-container">
+          <img :src="film.picUrl" alt="瞒天过海" title="点击看更多海报" class="movie-poster" />
+        </div>
+        <div class="info">
+          <div class="flex-item">
+            <span class="pl">类型:</span> <span>{{ film.genre }}</span>
+          </div>
+          <div class="flex-item">
+            <span class="pl">标签:</span> <span>{{ film.tags }}</span>
+          </div>
+          <div class="flex-item">
+            <span class="pl">制片国家/地区:</span>{{ film.country }}
+          </div>
+          <div class="summary">
+            {{ film.summary }}
+          </div>
+        </div>
       </div>
-      <div class="flex-item">
-        <span class="pl">标签:</span> <span>{{ film.tags }}</span>
-      </div>
-      <div class="flex-item">
-        <span class="pl">制片国家/地区:</span>{{ film.country }}
-      </div>
-      <div class="summary">
-        {{ film.summary }}
-      </div>
-    </div>
-  </div>
+      <div class="commentParent">
+        <div v-for="(item, index) in commentList" class="commentList">
+             <el-row>
+               <el-col :span="4">
+                <div class="comment-left inline-div ">
+                   <img src="../../../assets/avatar.jpg" class="avatar"/>
+                   <div class="nickname">{{ item.nickName }}</div>
+                </div>
+               </el-col>
+               <el-col :span="20">
+                <div class="comment-right inline-div ">
+                   <div class="comment">{{ item.comment }} </div>
+                 </div>
+               </el-col>
+             </el-row>
+              <div class="content">
 
-  <!--电影总结-->
+
+               </div>
+            </div>
+       </div>
+ </div>
+
 </template>
 
 
 <script>
+
 export default {
   name:"FilmCellDetail",
   data() {
     return {
       film: "",
+      commentList:[
+         {nickName:"某物ddd",comment:"我看了，觉得片子没他写的这么好而已。写的是真好，比片子还好。"},
+         {nickName:"某物某物某物某物",comment:"我看了，觉得片子没他写的这么好而已。写的是真好，比片子还好。"},
+         {nickName:"某物某物某物",comment:"我看了，觉得片子没他写的这么好而已。写的是真好，比片子还好。"},
+         {nickName:"某物",comment:"我看了，觉得片子没他写的这么好而已。写的是真好，比片子还好。"},
+         {nickName:"某物",comment:"我看了，觉得片子没他写的这么好而已。写的是真好，比片子还好。"},
+         {nickName:"某物",comment:"我看了，觉得片子没他写的这么好而已。写的是真好，比片子还好。"},
+         {nickName:"某物",comment:"我看了，觉得片子没他写的这么好而已。写的是真好，比片子还好。"},
+         {nickName:"某物",comment:"我看了，觉得片子没他写的这么好而已。写的是真好，比片子还好。"},
+         {nickName:"某物",comment:"我看了，觉得片子没他写的这么好而已。写的是真好，比片子还好。"},
+
+      ]
     };
   },
   computed: {
@@ -75,8 +110,13 @@ export default {
   margin-top: 46px;
  }
  .clearfix{
-  display: block;
- }
+  clear:both;
+  }
+ .clearfix::after {
+  content: "";
+  display: table;
+  clear: both;
+}
  .subjectwrap {
     position: none;
     float: none;
@@ -99,5 +139,40 @@ export default {
 .summary{
   padding: 11px 18px;
   text-align: left;
+}
+.commentParent{
+  margin-top: 40px;
+
+ }
+.commentList {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: left;
+}
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+.inline-div {
+  display: inline-block;
+}
+.comment-left{
+  margin-left: 17px;
+}
+.comment-right {
+  flex-grow: 1;
+  margin-left: 10px;
+  margin-top: -30px;
+}
+.comment{
+  min-height: 58.7px;
+  padding-top: 46px;
+
+}
+.nickname{
+  color: #999;
+  font-size: 14px;
+  margin-left: 3px;
 }
 </style>
